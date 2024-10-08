f335b65df8e85390824e39d27193be98822080a1
unknown <mariama428@gmail.com>
2024-10-08 11:22:48 +0300

    create backend project

---
+++ backend/spring-resturant/src/test/java/com/spring/resturant/SpringResturantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.resturant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResturantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
